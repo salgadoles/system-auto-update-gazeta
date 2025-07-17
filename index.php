5305e50ce57ddd98ef2a6696bf0290160992f582
Samuel Lima do Amaral <140810343+salgadoles@users.noreply.github.com>
2025-07-17 08:31:58 -0300

    Create base structure using bootstrap.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row-lg-4 offset-lg-4">
+            <form action="login.php" method="POST">
+                <div>
+                    <div class="mb-3">
+                        <label>usuario</label>
+                        <input type="text" name= "usuario" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-3">
+                        <label>senha</label>
+                        <input type="password" name= "senha" class="form-control">
+                    </div>
+                </div>
+                <div>
+                    <div class="mb-3">
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
