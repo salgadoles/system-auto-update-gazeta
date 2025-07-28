dfd46db857f588e0f4497309ea36ba3cb0269b29
Salgadoles <you@example.com>
2025-07-28 08:27:18 -0300

    Create forms structure inital.

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #f2f2f2;">
     <div class="container">
         <div class="row-lg-4 offset-lg-4">
-            <form action="login.php" method="POST">
+            <div class="card ">  
+                <div class="card-header"> <h3>Login</h3></div>
+          <div class="card-body">  
+           <form action="login.php" method="POST">
                 <div>
                     <div class="mb-3">
                         <label>usuario</label>
@@ -24,11 +27,15 @@
                 </div>
                 <div>
                     <div class="mb-3">
-
+                        <button type="submit" class="btn btn-primary">Enviar</button>
                     </div>
                 </div>
             </form>
+</div>
+</div>
         </div>
     </div>
 </body>
 </html>
+
+     
